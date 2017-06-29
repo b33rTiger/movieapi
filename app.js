b8bee35f3d3f10b45ab56b4c6b0a6fdfbf406da0
John <john@johnsheets.me>
2017-06-29 12:03:39 -0700

    search form working/display data working

--- app.js
+++ app.js
@@ -12,11 +12,12 @@ app.get("/", function(req, res){
 });
 
 app.get("/results", function(req, res){
-	request("http://www.omdbapi.com/?s=guardians+of+the+galaxy&apikey=thewdb", function(error, response, body){
+	var query = req.query.search;
+	var url = "http://www.omdbapi.com/?s=" + query + "&apikey=thewdb";
+	request(url, function(error, response, body){
 		if(!error && response.statusCode == 200) {
-			var results = JSON.parse(body);
-			console.log(results.Search.Title);
-			res.send(results.Search[0].Title);
+			var data = JSON.parse(body);
+			res.render("results", {data: data});
 		};
 	});
 });
